cfac16eade1149d58dae9b6b39e442dddb53b2ee
AndreBonafe <andre.bonaf@gmail.com>
2026-09-23 19:00:19 -0300

    fix: arruma a aria-current do header

--- DesenvolvimentoFrontEnd/Checkpoint-Literario/js/app.js
+++ DesenvolvimentoFrontEnd/Checkpoint-Literario/js/app.js
@@ -1,11 +1,23 @@
 import { router } from "./router.js";
 
+function updateHeaderAriaCurrent(route) {
+	console.log(route)
+	const validRoute = route || '#/inicio'
+	const links = document.querySelectorAll('.menu-principal a');
+
+	for (const link of links) {
+		const currentHref = link.getAttribute('href');
+		link.setAttribute('aria-current', currentHref === validRoute ? 'page' : '')
+	}
+}
+
 function renderizar() {
-    const app = document.querySelector("#app");
+	const app = document.querySelector("#app");
 
-    const rota = window.location.hash;
+	const rota = window.location.hash;
 
-    app.innerHTML = router(rota);
+	app.innerHTML = router(rota);
+	updateHeaderAriaCurrent(rota);
 }
 
 window.addEventListener("DOMContentLoaded", renderizar);
