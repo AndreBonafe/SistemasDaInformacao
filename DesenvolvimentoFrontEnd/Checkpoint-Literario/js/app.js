6c3139c3b838945f71c86c819e92f895e340f1b8
AndreBonafe <andre.bonaf@gmail.com>
2026-09-23 18:45:04 -0300

    chore: cria router

--- DesenvolvimentoFrontEnd/Checkpoint-Literario/js/app.js
+++ DesenvolvimentoFrontEnd/Checkpoint-Literario/js/app.js
@@ -1,19 +1,11 @@
-import { Cadastro } from "./views/cadastro.js";
-import { Home } from "./views/home.js";
-import { Projetos } from "./views/projetos.js";
+import { router } from "./router.js";
 
 function renderizar() {
     const app = document.querySelector("#app");
 
     const rota = window.location.hash;
 
-    if (rota === "#/projetos") {
-        app.innerHTML = Projetos();
-    } else if (rota === "#/cadastro") {
-        app.innerHTML = Cadastro();
-    } else {
-        app.innerHTML = Home();
-    }
+    app.innerHTML = router(rota);
 }
 
 window.addEventListener("DOMContentLoaded", renderizar);
---
+++ DesenvolvimentoFrontEnd/Checkpoint-Literario/js/router.js
@@ -0,0 +1,14 @@
+import { Cadastro } from "./views/cadastro.js";
+import { Home } from "./views/home.js";
+import { Projetos } from "./views/projetos.js";
+
+const routes = {
+  "#/inicio": Home,
+  "#/cadastro": Cadastro,
+  "#/projetos": Projetos
+}
+
+export function router(rota) {
+  const view = routes[rota];
+  return view ? view() : Home();
+}
